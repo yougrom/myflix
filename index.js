516852ecd553e593da7eb55ffab945fd90db0eeb
Herr Gromov <youriy.gromov@gmail.com>
2024-03-26 17:50:29 +0100

    del site URL to CORS allowed origins

--- index.js
+++ index.js
@@ -38,23 +38,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // CORS
 const cors = require('cors');
 app.use(cors());
-let allowedOrigins = [
-  'http://localhost:8080', 
-  'http://testsite.com', 
-  'http://localhost:1234', 
-  'https://myflix-gromov.netlify.app'
-];
+// let allowedOrigins = [
+//   'http://localhost:8080', 
+//   'http://testsite.com', 
+//   'http://localhost:1234', 
+//   'https://myflix-gromov.netlify.app'
+// ];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 
